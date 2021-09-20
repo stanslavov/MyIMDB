d95669df3a7da26082a5c9374fb2d2b2b91d4f1b
Stanislav Slavov <stanislav.slavov88@gmail.com>
2021-09-20 19:20:30 +0300

    initial commit

---
+++ Web/MyIMDB.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,14 @@
+﻿namespace MyIMDB.Web.Areas.Administration.Controllers
+{
+    using MyIMDB.Common;
+    using MyIMDB.Web.Controllers;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [Area("Administration")]
+    public class AdministrationController : BaseController
+    {
+    }
+}
---
+++ Web/MyIMDB.Web/Controllers/BaseController.cs
@@ -0,0 +1,8 @@
+﻿namespace MyIMDB.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BaseController : Controller
+    {
+    }
+}
